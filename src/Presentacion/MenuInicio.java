6a2a4c18014c58bfbd1f112a81c3dfd8b772ff11
DieterPreuss <87830546+DieterPreuss@users.noreply.github.com>
2021-08-21 18:11:31 -0300

    Cambios menores

--- src/Presentacion/MenuInicio.java
+++ src/Presentacion/MenuInicio.java
@@ -289,15 +289,8 @@ public class MenuInicio extends javax.swing.JFrame {
         this.jLabel1.setVisible(false);
     }//GEN-LAST:event_jMenuItem11ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -314,9 +307,7 @@ public class MenuInicio extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(MenuInicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new MenuInicio().setVisible(true);
